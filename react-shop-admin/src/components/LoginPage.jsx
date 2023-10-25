ff71b70f79ed421bcb2e0d79c560b4d0fc8ff09f
= <=>
2023-10-25 18:00:39 -0300

    authenticating in the api using axios

--- react-shop-admin/src/components/LoginPage.jsx
+++ react-shop-admin/src/components/LoginPage.jsx
@@ -1,13 +1,20 @@
 import { useRef } from 'react';
 import { LockClosedIcon } from '@heroicons/react/20/solid';
+import { useAuth } from '@hooks/useAuth';
 
 export default function LoginPage() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+  const auth = useAuth();
+
   const handleSubmit = (e) =>{
     e.preventDefault();
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
+
+    auth.signIn(email,password).then(()=>{
+      console.log('login success')
+    })
   }
   return (
     <>
--- react-shop-admin/src/hooks/useAuth.js
+++ react-shop-admin/src/hooks/useAuth.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext, createContext } from 'react';
 import Cookies from 'js-cookie';
 import axios from 'axios';
+import endPoints from '@services/api';
 
 const authContext = createContext();
 
@@ -17,7 +18,13 @@ function useProviderAuth() {
   const [user, setUser] = useState(null);
 
   const signIn = async (email, password) => {
-    setUser('login');
+    const options = {
+      Headers: {
+        accept: '*/*',
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data: acces_token } = await axios.post(endPoints.auth.login, { email, password }, options);
   };
   return {
     user,
