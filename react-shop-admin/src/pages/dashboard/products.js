def039adb18ccdaa8410f0aaacd2a8c3151c64f9
= <=>
2023-11-03 01:00:42 -0300

    Construction of the modal to create products

--- react-shop-admin/src/pages/dashboard/products.js
+++ react-shop-admin/src/pages/dashboard/products.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { CheckIcon } from '@heroicons/react/20/solid';
 import Modal from '@common/Modal';
+import FormProduct from '@components/FormProduct';
 
 export default function products() {
   const [open, setOpen] = useState(false);
@@ -90,7 +91,7 @@ export default function products() {
         </div>
       </div>
       <Modal open={open} setOpen={setOpen}>
-        <h1>Hola mundo</h1>
+        <FormProduct />
       </Modal>
     </>
   );
